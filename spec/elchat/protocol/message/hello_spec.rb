4704e9dece3bdb7f561f595a57bfaa7b8147b6e1
Karol Sarnacki <sodercober@gmail.com>
2013-10-26 01:37:43 +0200

    Refactor messages specs

--- spec/elchat/protocol/message/hello_spec.rb
+++ spec/elchat/protocol/message/hello_spec.rb
@@ -2,7 +2,8 @@ require_relative '../../../spec_helper'
 
 describe ElChat::Protocol::Message::Hello do
   before do
-    @hello = ElChat::Protocol::Message::Hello.new
+    @hello       = ElChat::Protocol::Message::Hello.new
+    @hello_bytes = "{\x00\x11\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"
   end
 
   it 'initiates with default version number' do
@@ -13,19 +14,19 @@ describe ElChat::Protocol::Message::Hello do
     it 'returns payload bytes' do
       @hello.protocol_version = 123
       @hello.port             = 17
-      @hello.nonce            = 18446744073709551616
+      @hello.nonce            = 2**64 - 1
 
-      @hello.payload.must_equal "{\x00\x11\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+      @hello.payload.must_equal @hello_bytes
     end
   end
 
   describe '#extract' do
     it 'extracts payload bytes to object values' do
-      @hello.extract("\x94&\xE4\a\x12!\x9A\x00\x00\x00\x00\x00")
+      @hello.extract @hello_bytes
 
-      @hello.protocol_version.must_equal 9876
-      @hello.port.must_equal 2020
-      @hello.nonce.must_equal 10101010
+      @hello.protocol_version.must_equal 123
+      @hello.port.must_equal 17
+      @hello.nonce.must_equal 2**64 - 1
     end
   end
 
--- spec/elchat/protocol/message/peer_info_spec.rb
+++ spec/elchat/protocol/message/peer_info_spec.rb
@@ -2,24 +2,25 @@ require_relative '../../../spec_helper'
 
 describe ElChat::Protocol::Message::PeerInfo do
   before do
-    @peer_info = ElChat::Protocol::Message::PeerInfo.new
+    @peer_info       = ElChat::Protocol::Message::PeerInfo.new
+    @peer_info_bytes = "\x01\x02\x03\x04\xFF\xFF\xAF5\xA24"
   end
 
   describe '#payload' do
     it 'returns payload bytes' do
       @peer_info.ip           = '1.2.3.4'
-      @peer_info.port         = 5678
-      @peer_info.last_seen_at = Time.new(2013, 10, 25, 23, 46, 27)
+      @peer_info.port         = 65535
+      @peer_info.last_seen_at = Time.new(1997, 12, 25, 11, 30, 7)
 
-      @peer_info.payload.must_equal "\x01\x02\x03\x04.\x16\xB3\xE6jR"
+      @peer_info.payload.must_equal @peer_info_bytes
     end
   end
 
   describe '#extract' do
     it 'extracts payload bytes to object values' do
-      @peer_info.extract("\x00\x00\x00\x00\xFF\xFF\xAF5\xA24")
+      @peer_info.extract @peer_info_bytes
 
-      @peer_info.ip.must_equal '0.0.0.0'
+      @peer_info.ip.must_equal '1.2.3.4'
       @peer_info.port.must_equal 65535
       @peer_info.last_seen_at.must_equal Time.new(1997, 12, 25, 11, 30, 7)
     end
