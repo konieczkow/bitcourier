32bbaa9e160122226d585c3314ba4e55d6e972d9
Karol Sarnacki <sodercober@gmail.com>
2013-10-25 20:03:04 +0200

    Refactor ElChat::Client

--- elchat/network/client.rb
+++ elchat/network/client.rb
@@ -2,6 +2,8 @@ module ElChat
   module Network
 
     class Client
+      PEER_CONNECTION_RETRY_DELAY = 5 # In seconds
+      NEXT_PEER_DELAY             = 5
 
       def initialize context
         @context = context
@@ -11,13 +13,13 @@ module ElChat
         @thread = Thread.new do
           loop do
             if @context.node_manager.needs_nodes?
-              if conn = new_connection
-                @context.node_manager.add_socket conn, true
+              if peer_connection = next_peer_connection
+                @context.node_manager.add_socket peer_connection, true
               else
-                sleep(5000)
+                sleep(NEXT_PEER_DELAY)
               end
             else
-              sleep(5000)
+              sleep(NEXT_PEER_DELAY)
             end
           end
         end
@@ -25,26 +27,24 @@ module ElChat
 
       private
 
-      def new_connection
-        peer = @context.peer_list.next
+      def next_peer_connection
+        if peer = @context.peer_list.next
+          puts "Connecting to peer #{peer[0]} on port #{peer[1]}"
 
-        if peer
-          ip, port = peer[0], peer[1]
-        else
-          ip, port = '127.0.0.1', $client_port
+          socket = TCPSocket.new(peer[0], peer[1])
+
+          @context.peer_list.store(peer[0], peer[1], Time.now, nil)
+
+          socket
         end
-
-        puts "Connecting to peer #{ip} on port #{port}"
-
-        TCPSocket.new(ip, port)
       rescue Errno::ECONNREFUSED
-        puts "Connection refused"
+        puts 'Connection refused'
 
-        @context.peer_list.store(peer[0], peer[1], peer[2], Time.now + 60 * 10)
+        @context.peer_list.store(peer[0], peer[1], peer[2], Time.now + PEER_CONNECTION_RETRY_DELAY)
 
         nil
       end
-
+      
     end
 
   end
