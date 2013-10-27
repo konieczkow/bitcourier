27d5ca5a924900d819ccf13a8d386fd0d24fd28d
Karol Sarnacki <sodercober@gmail.com>
2013-10-27 12:09:31 +0100

    Allow to set minimum_nodes when using CLI

--- lib/bitcourier/command.rb
+++ lib/bitcourier/command.rb
@@ -6,13 +6,18 @@ module Bitcourier
 
     desc 'node', 'Start Bitcourier node'
     method_option :port,
-                  desc:    'port on which the Bitcourier node will listen',
-                  default: Network::Server::DEFAULT_PORT,
-                  aliases: '-p',
-                  type:    :numeric
+                  :desc    => 'port on which the Bitcourier node will listen',
+                  :default => Network::Server::DEFAULT_PORT,
+                  :aliases => '-p',
+                  :type    => :numeric
+    method_option :minimum_nodes,
+                  :desc    => 'minimum number of nodes to which Bitcourier will connect',
+                  :default => NodeManager::DEFAULT_MINIMUM_NODES,
+                  :aliases => '-n',
+                  :type    => :numeric
 
     def node
-      Daemon.new.run port: options.port
+      Daemon.new.run :port => options.port
     end
 
   end
--- lib/bitcourier/node_manager.rb
+++ lib/bitcourier/node_manager.rb
@@ -1,16 +1,19 @@
 module Bitcourier
   class NodeManager
-    MINIMUM_NODES = 3
+    DEFAULT_MINIMUM_NODES = 3
 
-    attr_accessor :context
+    attr_reader :context
+    attr_accessor :minimum_nodes
 
-    def initialize context
-      self.nodes   = []
-      self.context = context
+    def initialize(context, options = {})
+      @nodes   = []
+      @context = context
+
+      self.minimum_nodes = options.fetch(:minimum_nodes, DEFAULT_MINIMUM_NODES)
     end
 
     def needs_nodes?
-      nodes.count < MINIMUM_NODES
+      nodes.count < minimum_nodes
     end
 
     def add_socket socket, active = false
@@ -29,7 +32,7 @@ module Bitcourier
 
     private
 
-    attr_accessor :nodes
+    attr_reader :nodes
 
   end
 end
