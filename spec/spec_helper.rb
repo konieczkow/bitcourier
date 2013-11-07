a976a05214558d85e109631f297c5e32775403ee
Karol Sarnacki <sodercober@gmail.com>
2013-11-07 21:27:48 +0100

    Fix Simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 gem 'minitest' # ensure we are using the gem version
 
 require 'bitcourier'
 require 'minitest/autorun'
 require 'json' # https://github.com/colszowka/simplecov/issues/249
-require 'simplecov'
-
-SimpleCov.start
