63af80d32d79d2fff452574628bf9c09c283d9c7
Karol Sarnacki <sodercober@gmail.com>
2013-11-09 14:23:12 +0100

    Fix specs to pass in other time zone

--- spec/bitcourier/peer_spec.rb
+++ spec/bitcourier/peer_spec.rb
@@ -5,12 +5,12 @@ describe Bitcourier::Peer do
 
   describe '::from_a' do
     it 'returns array representation of the peer' do
-      @peer = Bitcourier::Peer.from_a(['1.2.3.4', 5678, 1383999085, 1393683445])
+      @peer = Bitcourier::Peer.from_a(['1.2.3.4', 5678, 1384002685, 1393687045])
 
       @peer.ip.must_equal '1.2.3.4'
       @peer.port.must_equal 5678
-      @peer.last_seen_at.must_equal Time.local(2013, 11, 9, 13, 11, 25)
-      @peer.next_connection_at.must_equal Time.local(2014, 3, 1, 15, 17, 25)
+      @peer.last_seen_at.must_equal Time.utc(2013, 11, 9, 13, 11, 25)
+      @peer.next_connection_at.must_equal Time.utc(2014, 3, 1, 15, 17, 25)
     end
   end
 
@@ -27,7 +27,7 @@ describe Bitcourier::Peer do
   describe '#touch' do
     before do
       @peer = Bitcourier::Peer.new('1.2.3.4', 5678)
-      @now  = Time.local(2013, 11, 9, 12, 39, 47)
+      @now  = Time.utc(2013, 11, 9, 12, 39, 47)
 
       Timecop.freeze(@now)
     end
@@ -41,48 +41,48 @@ describe Bitcourier::Peer do
 
     describe 'when last_seen_at is not present' do
       it 'updates last_seen_at to given value' do
-        @peer.touch(Time.local(2013, 11, 9, 13, 03, 11))
-        @peer.last_seen_at.must_equal Time.local(2013, 11, 9, 13, 03, 11)
+        @peer.touch(Time.utc(2013, 11, 9, 13, 03, 11))
+        @peer.last_seen_at.must_equal Time.utc(2013, 11, 9, 13, 03, 11)
       end
     end
 
     describe 'when given value is greater than existing last_seen_at' do
       it 'updates last_seen_at to given value' do
-        @peer.last_seen_at = Time.local(2011, 11, 9, 13, 03, 11)
-        @peer.touch(Time.local(2013, 11, 9, 13, 04, 21))
-        @peer.last_seen_at.must_equal Time.local(2013, 11, 9, 13, 04, 21)
+        @peer.last_seen_at = Time.utc(2011, 11, 9, 13, 03, 11)
+        @peer.touch(Time.utc(2013, 11, 9, 13, 04, 21))
+        @peer.last_seen_at.must_equal Time.utc(2013, 11, 9, 13, 04, 21)
       end
     end
 
     describe 'when given value is less than or equal to existing last_seen_at' do
       it 'does not update last_seen_at' do
-        @peer.last_seen_at = Time.local(2013, 11, 9, 12, 39, 47)
-        @peer.touch(Time.local(2013, 10, 9, 12, 39, 47))
-        @peer.last_seen_at.must_equal Time.local(2013, 11, 9, 12, 39, 47)
+        @peer.last_seen_at = Time.utc(2013, 11, 9, 12, 39, 47)
+        @peer.touch(Time.utc(2013, 10, 9, 12, 39, 47))
+        @peer.last_seen_at.must_equal Time.utc(2013, 11, 9, 12, 39, 47)
       end
     end
   end
 
   describe '#update' do
     before do
-      @peer1 = Bitcourier::Peer.new('1.2.3.4', 5678, Time.local(2011, 10, 3, 8, 21, 3), nil)
-      @peer2 = Bitcourier::Peer.new('1.2.3.4', 5678, Time.local(2013, 11, 9, 13, 11, 25), Time.local(2014, 3, 1, 15, 17, 25))
+      @peer1 = Bitcourier::Peer.new('1.2.3.4', 5678, Time.utc(2011, 10, 3, 8, 21, 3), nil)
+      @peer2 = Bitcourier::Peer.new('1.2.3.4', 5678, Time.utc(2013, 11, 9, 13, 11, 25), Time.utc(2014, 3, 1, 15, 17, 25))
 
       @peer1.update(@peer2)
     end
 
     it 'updates last_seen_at to the value from given peer' do
-      @peer1.last_seen_at.must_equal Time.local(2013, 11, 9, 13, 11, 25)
+      @peer1.last_seen_at.must_equal Time.utc(2013, 11, 9, 13, 11, 25)
     end
 
     it 'updates next_connection_at to the value from given peer' do
-      @peer1.next_connection_at.must_equal Time.local(2014, 3, 1, 15, 17, 25)
+      @peer1.next_connection_at.must_equal Time.utc(2014, 3, 1, 15, 17, 25)
     end
   end
 
   describe '#can_connect?' do
     before do
-      @now    = Time.local(2013, 11, 9, 13, 37, 53)
+      @now    = Time.utc(2013, 11, 9, 13, 37, 53)
       @past   = @now - 1
       @future = @now + 1
 
@@ -110,7 +110,7 @@ describe Bitcourier::Peer do
 
   describe '#retry_in' do
     before do
-      @now  = Time.local(2013, 11, 9, 13, 37, 51)
+      @now  = Time.utc(2013, 11, 9, 13, 37, 51)
       @peer = Bitcourier::Peer.new('1.2.3.4', 5678)
 
       Timecop.freeze(@now)
@@ -119,15 +119,15 @@ describe Bitcourier::Peer do
     it 'sets next_connection_at to given seconds in the future' do
       @peer.retry_in(7)
 
-      @peer.next_connection_at.must_equal Time.local(2013, 11, 9, 13, 37, 58)
+      @peer.next_connection_at.must_equal Time.utc(2013, 11, 9, 13, 37, 58)
     end
   end
 
   describe '#to_a' do
     it 'returns array representation of the peer' do
-      @peer = Bitcourier::Peer.new('1.2.3.4', 5678, Time.local(2013, 11, 9, 13, 11, 25), Time.local(2014, 3, 1, 15, 17, 25))
+      @peer = Bitcourier::Peer.new('1.2.3.4', 5678, Time.utc(2013, 11, 9, 13, 11, 25), Time.utc(2014, 3, 1, 15, 17, 25))
 
-      @peer.to_a.must_equal ['1.2.3.4', 5678, 1383999085, 1393683445]
+      @peer.to_a.must_equal ['1.2.3.4', 5678, 1384002685, 1393687045]
     end
   end
 
