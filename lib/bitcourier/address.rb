7114e02dc3d5b228ef81d1010860859e2bed5576
Karol Sarnacki <sodercober@gmail.com>
2013-11-09 14:38:42 +0100

    Simplify Address class by using Struct

--- lib/bitcourier/address.rb
+++ lib/bitcourier/address.rb
@@ -1,22 +1,14 @@
 module Bitcourier
-  class Address
-    attr_reader :ip, :port
+  class Address < Struct.new(:ip, :port)
+    private :ip=, :port=
 
     def initialize(ip, port)
-      @ip   = ip   || raise(ArgumentError, 'IP must be present')
-      @port = port || raise(ArgumentError, 'port must be present')
-    end
-
-    def ==(other)
-      equal?(other) || (other.instance_of?(self.class) && (other.ip == ip) && (other.port == port))
+      self.ip   = ip   || raise(ArgumentError, 'IP must be present')
+      self.port = port || raise(ArgumentError, 'port must be present')
     end
 
     def to_s
       "#{ip}:#{port}"
-    end
-
-    def to_a
-      [ip, port]
     end
 
   end
